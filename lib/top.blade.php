bfc553239d5b509af9deb9b5246b7a139b2d7f33
Sherzot Musurmonov <102805587+sherzot@users.noreply.github.com>
2025-04-13 19:38:35 +0900

    Update top.blade.php

--- lib/top.blade.php
+++ lib/top.blade.php
@@ -10,10 +10,52 @@
         <!-- Favicon & Icons -->
         <link rel="icon" href="{{ asset('assets/img/favicon.png') }}">
         <link rel="apple-touch-icon" href="{{ asset('assets/img/icon.png') }}">
-        <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
+        {{--  <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">  --}}
         <!-- CDN (6.4.2 ishlovchi versiya) -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+        {{--  GOOGLE FONT  --}}
+        {{--  @vite(['resources/css/app.css', 'resources/js/app.js'])  --}}
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"
+            rel="stylesheet">
+        <script>
+            window.dataLayer = window.dataLayer || [];
 
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', 'G-6YVRJF2XSX', {
+                'linker': {
+                    'domains': ['shigotonavi.co.jp', 'mch.shigotonavi.co.jp']
+                },
+                'cookie_domain': 'auto',
+                'cookie_flags': 'SameSite=None;Secure'
+            });
+        </script>
+
+
+
+
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({
+                    'gtm.start': new Date().getTime(),
+                    event: 'gtm.js'
+                });
+                var f = d.getElementsByTagName(s)[0],
+                    j = d.createElement(s),
+                    dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src =
+                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-WRJGM8M8');
+        </script>
+        <!-- End Google Tag Manager -->
         <!-- Bootstrap -->
         <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
@@ -25,7 +67,7 @@
         @livewireStyles
 
         <style>
-            @font-face {
+            {{--  @font-face {
                 font-family: 'NotoSansJP';
                 src: url('/fonts/noto/NotoSansJP-Regular.ttf') format('truetype');
                 font-weight: normal;
@@ -37,16 +79,16 @@
                 src: url('/fonts/noto/NotoSansJP-Bold.ttf') format('truetype');
                 font-weight: bold;
                 font-style: normal;
-            }
+            }  --}}
 
             html,
             body {
-                font-family: 'NotoSansJP', Helvetica, Arial, sans-serif;
+                font-family: 'Noto Sans JP', sans-serif;
                 font-size: var(--bs-body-font-size);
                 font-weight: var(--bs-body-font-weight);
                 line-height: var(--bs-body-line-height);
                 margin: 0;
-                {{--  padding-bottom: 50px;  --}} height: 100%;
+                height: 100%;
                 scroll-behavior: smooth;
                 overflow-x: hidden !important;
             }
@@ -61,6 +103,14 @@
             }
 
             .btn-main-theme:hover {
+                background-color: #FF4500 !important;
+            }
+            .btn-main-theme2 {
+                background-color: #bc1c45 !important;
+                color: white !important;
+            }
+
+            .btn-main-theme2:hover {
                 background-color: #FF4500 !important;
             }
 
@@ -102,16 +152,12 @@
                 width: 60px;
                 height: 60px;
                 border-radius: 50%;
-                {{--  background: linear-gradient(135deg, #FF6347, #FF4500);  --}} {{--  color: white;  --}} font-size: 22px;
+                font-size: 22px;
                 display: flex;
                 align-items: center;
                 justify-content: center;
                 z-index: 1060;
                 cursor: pointer;
-                {{--  opacity: 0.8;
-                animation: pulse 2s infinite;
-                transition: all 0.3s ease-in-out;
-                box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);  --}}
             }
 
             #scroll-top img {
@@ -124,16 +170,19 @@
                 transform: scale(1.1);
                 animation: none;
             }
+
             @media screen and (max-width: 500px) {
                 #scroll-top {
                     right: 10px;
                     bottom: 50px;
                 }
-                .fs-f28{
+
+                .fs-f28 {
                     font-size: 18px !important;
                     line-height: 1.6 !important;
                     text-align: center !important;
                 }
+
                 .fs-f18 {
                     font-size: 14px !important;
                     line-height: 1.6 !important;
@@ -141,9 +190,9 @@
                     padding-right: 10px !important;
                     text-align: center !important;
                 }
-            
+
                 .text-center {
-                    text-align: center !important; /* mobilda chapga */
+                    text-align: center !important;
                 }
             }
         </style>
@@ -169,8 +218,7 @@
                     <a href="{{ route('company.login') }}" class="btn btn-outline-light btn-sm">企業</a>
                     <a href="{{ route('contact.form') }}" class="btn btn-outline-light btn-sm">お問い合わせ</a>
                 </div>
-                <a href="https://www.shigotonavi.co.jp/privacy/privacymark.asp" target="_blank"
-                    class="d-inline-block text-white text-decoration-none">
+                <a href="#" target="_blank" class="d-inline-block text-white text-decoration-none">
                     <div class="small">しごとナビ利用規約・<br class="d-sm-none">個人情報保護に関する事項</div>
                 </a>
             </div>
